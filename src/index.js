25f7b0b4dd3a672162f1a731ac98aec7a791bd57
Tari20691 <150802689+Tari20691@users.noreply.github.com>
2023-11-24 08:06:27 +1100

    Added surch functionality using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function refreshWeather(response){//4. response von refreshWeather
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature); // temperstur runden
+    }
+function searchCity(city){    //1. make api call and update interface
+    let apiKey = "o1b51294bd100044e1tab17f08833d34";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);// 3. hier bekommen wir die URL über axios und dann für refreshWeather eine function machen
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    
+    searchCity(searchInput.value); //2. someone searches on my website the city it is going to call the handleSearchSubmit function and submit it, und dann der value of search input is going to the searchCity function, that search City funtion is getting a call now, what does it with it?
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Berlin");
